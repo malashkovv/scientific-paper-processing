4377680ad5f5d84faac959fe670fe10813d768b7
Valentin Malashkov <malashkovv@gmail.com>
2019-11-20 16:22:54 +0300

    Fixes

--- scrapper/scrapper/settings.py
+++ scrapper/scrapper/settings.py
@@ -10,18 +10,15 @@ ROBOTSTXT_OBEY = True
 
 DOWNLOAD_DELAY = 0.5
 
-# EXTENSIONS = {
-#     'scrapy_kafka_export.KafkaItemExporterExtension': 1,
-# }
-# KAFKA_EXPORT_ENABLED = True
-# KAFKA_BROKERS = [
-#     'kafka1:9092',
-#     'kafka2:9092'
-# ]
-# KAFKA_TOPIC = 'science-papers'
+EXTENSIONS = {
+    'scrapy_kafka_export.KafkaItemExporterExtension': 1,
+}
+
+KAFKA_EXPORT_ENABLED = True
+KAFKA_BROKERS = os.environ.get("KAFKA_BROKERS", 'kafka1:9092,kafka2:9092').splot(',')
+KAFKA_TOPIC = os.environ.get("KAFKA_TOPIC", 'science-papers')
 
 BIORXIV_CATEGORIES = 'pharmacology-and-toxicology,cell-biology,cancer-biology,genetics,neuroscience,bioinformatics'.split(',')
-# os.environ.get('BIORXIV_CATEGORY', 'bioinformatics').split(",")
 BIORXIV_START_PAGE = int(os.environ.get('BIORXIV_START_PAGE', '0'))
 
 BIORXIV_END_PAGE = os.environ.get('BIORXIV_END_PAGE', None)
